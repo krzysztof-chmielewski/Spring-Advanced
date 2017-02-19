d468b634316e364a9cd751c0f6b2242cc5d767ca
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-02-19 05:01:38 +0100

    Fixes controller mapping

--- 03 REST/src/main/java/io/kch/sda/spring/rest03/song/SongController.java
+++ 03 REST/src/main/java/io/kch/sda/spring/rest03/song/SongController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "songs/")
+@RequestMapping(path = "songs")
 public class SongController {
     private final MusicPlayer musicPlayer;
 
--- 04 REST/src/main/java/io/kch/sda/spring/rest04/song/SongController.java
+++ 04 REST/src/main/java/io/kch/sda/spring/rest04/song/SongController.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "songs/")
+@RequestMapping(path = "songs")
 public class SongController {
     private final MusicPlayer musicPlayer;
 
